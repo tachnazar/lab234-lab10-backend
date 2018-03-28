be4af1b230b929c1768d69d90111956dfc32c254
Unknown <tachmuhammet96@gmail.com>
2018-03-28 17:57:04 +0700

    Done!

--- src/main/java/camt/se234/lab10/service/GradeServiceImpl.java
+++ src/main/java/camt/se234/lab10/service/GradeServiceImpl.java
@@ -7,15 +7,15 @@ public class GradeServiceImpl implements GradeService {
     public String getGrade(double score) {
        if(score<=100 && score>=80){
            return "A";
-       }else if(score<=78.9 && score>=75){
+       }else if(score<80 && score>=75){
            return "B";
-       }else if(score<=74.4 && score>=60){
+       }else if(score<75 && score>=60){
            return "C";
-       }else if(score<=59.4 && score>=33){
+       }else if(score<60 && score>=33){
            return "D";
-       }else if(score<=32 && score>=0){
+       }else if(score<33 && score>=0){
            return "F";
        }
-        return "F";
+        return "Invalid";
     }
 }
